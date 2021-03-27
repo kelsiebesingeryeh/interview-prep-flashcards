7be04c64bb35c0a43d208339a39c22f182c2878f
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-03-27 10:30:28 -0600

    Add data file for interview questions

---
+++ src/Data/flashcardsData.js
@@ -0,0 +1,442 @@
+export const flashcardsData = [
+  {
+    id: 1,
+    category: "technical",
+    subCategory: "Computer Science",
+    question:
+      "In as much detail as possible, explain how you would localize an application.",
+  },
+  {
+    id: 2,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "Tell me two advantages of testing your code.",
+  },
+  {
+    id: 3,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "Name three strategies for fixing cross-browser inconsistencies.",
+  },
+  {
+    id: 4,
+    category: "technical",
+    subCategory: "Computer Science",
+    question:
+      "What are some tools and strategies you use to prevent shipping unstable code to production?",
+  },
+  {
+    id: 5,
+    category: "technical",
+    subCategory: "Computer Science",
+    question:
+      "What factors influence whether you’ll take a progressive enhancement vs. graceful degradation approach to building an application?",
+  },
+  {
+    id: 6,
+    category: "technical",
+    subCategory: "Computer Science",
+    question:
+      "Define the term ‘MVC’ and explain how an application is architected when following MVC patterns.",
+  },
+  {
+    id: 7,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "What does CORS stand for and what issue does it address?",
+  },
+  {
+    id: 8,
+    category: "technical",
+    subCategory: "Computer Science",
+    question:
+      "In as much detail as possible, describe the request-response cycle.",
+  },
+  {
+    id: 9,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "Tell me 3 new features of CSS3.",
+  },
+  {
+    id: 10,
+    category: "technical",
+    subCategory: "Computer Science",
+    question:
+      "Can you describe what responsive design is to you and how you would implement it?",
+  },
+  {
+    id: 11,
+    category: "technical",
+    subCategory: "Computer Science",
+    question:
+      "What’s the difference between display: inline and display: inline-block?",
+  },
+  {
+    id: 12,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "What is a pseudo class? What are they used for?",
+  },
+  {
+    id: 13,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "Describe z-index and how stacking context is formed.",
+  },
+  {
+    id: 14,
+    category: "technical",
+    subCategory: "Computer Science",
+    question:
+      "If you have two elements inside of an outer containing element, one with float: left; and the other with float: right, how can you ensure that the containing element expands around the floated elements and does not collapse?",
+  },
+  {
+    id: 15,
+    category: "technical",
+    subCategory: "Computer Science",
+    question:
+      "Why is it, in general, a good idea to leave the global scope of a website as-is and never touch it?",
+  },
+  {
+    id: 16,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "What does event bubbling or event propagation mean?",
+  },
+  {
+    id: 17,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "What’s the difference between undefined and null",
+  },
+  {
+    id: 18,
+    category: "technical",
+    subCategory: "Computer Science",
+    question:
+      "In as much detail as possible, explain how JSON Web Tokens work.",
+  },
+  {
+    id: 19,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "What is Ajax?",
+  },
+  {
+    id: 20,
+    category: "technical",
+    subCategory: "Computer Science",
+    question:
+      "What is use strict? What are the advantages and disadvantages to using it?",
+  },
+  {
+    id: 21,
+    category: "technical",
+    subCategory: "Computer Science",
+    question:
+      "Explain why the following doesn’t work as an IIFE: function foo(){ }();. What needs to be changed to properly make it an IIFE? Why?",
+  },
+  {
+    id: 22,
+    category: "technical",
+    subCategory: "Computer Science",
+    question:
+      "What are the pros and cons of using Promises instead of callbacks?",
+  },
+  {
+    id: 23,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "What is a closure, and how/why would you use one?",
+  },
+  {
+    id: 24,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "What advantages does React offer? What about disadvantages?",
+  },
+  {
+    id: 25,
+    category: "technical",
+    subCategory: "Computer Science",
+    question:
+      "Why is it generally a good idea to position CSS <link>s between <head></head> and JS <script>s just before </body>? Do you know any exceptions?",
+  },
+  {
+    id: 26,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "In an HTML file, what does the ‘doctype’ keyword do?",
+  },
+  {
+    id: 27,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "Give an example of a self-closing HTML tag.",
+  },
+  {
+    id: 28,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "What’s the difference between window.onload and onDocumentReady",
+  },
+  {
+    id: 29,
+    category: "technical",
+    subCategory: "Computer Science",
+    question:
+      "Give an example of an element that is considered a ‘block-level’ element? An example of an inline element? What’s the difference between block-level and inline elements?",
+  },
+  {
+    id: 30,
+    category: "technical",
+    subCategory: "Computer Science",
+    question:
+      "What could we use instead of <b> tags for bold and <i> tags for italics to make our HTML more semantic?",
+  },
+  {
+    id: 31,
+    category: "technical",
+    subCategory: "Computer Science",
+    question:
+      "What is the purpose of article, section, header and footer tags? Please explain with an example and why we should not use divs.",
+  },
+  {
+    id: 32,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "What are HTML data attributes?",
+  },
+  {
+    id: 33,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "What is the event loop?",
+  },
+  {
+    id: 34,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "What are some popular NodeJS Modules?",
+  },
+  {
+    id: 35,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "What is the concept of state in React?",
+  },
+  {
+    id: 36,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "What is the virtual DOM in React?",
+  },
+  {
+    id: 37,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "How is an array different from an object?",
+  },
+  {
+    id: 38,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "What is the DOM? How is the virtual dom different?",
+  },
+  {
+    id: 39,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "What does npm eject do?",
+  },
+  {
+    id: 40,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "What does npm build do?",
+  },
+  {
+    id: 41,
+    category: "technical",
+    subCategory: "Computer Science",
+    question:
+      "How do you make sure your project meets the requirements you have received?",
+  },
+  {
+    id: 42,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "What IDE do you use?",
+  },
+  {
+    id: 43,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "How does json work?",
+  },
+  {
+    id: 44,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "What is a restful api?",
+  },
+  {
+    id: 45,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "What is a react component?",
+  },
+  {
+    id: 46,
+    category: "technical",
+    subCategory: "Computer Science",
+    question:
+      "Whats the difference between map state to props and map dispatch to props?",
+  },
+  {
+    id: 47,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "What problems does redux solve?",
+  },
+  {
+    id: 48,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "What are libraries you have used?",
+  },
+  {
+    id: 49,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "What are frameworks you have used?",
+  },
+  {
+    id: 50,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "How are props different from state?",
+  },
+  {
+    id: 51,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "Do you know the concept of SCRUM?",
+  },
+  {
+    id: 52,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "What is JSX?",
+  },
+  {
+    id: 53,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "How is JSX different from HTML?",
+  },
+  {
+    id: 54,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "Explain how a branch works",
+  },
+  {
+    id: 55,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "What can you tell me about Rest APIs?",
+  },
+  {
+    id: 56,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "Do you know what a bearer token is in terms of JWT?",
+  },
+  {
+    id: 57,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "What is CORS?",
+  },
+  {
+    id: 58,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "Whats the difference between ES5 and ES6?",
+  },
+  {
+    id: 59,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "What method was you use to search an array and return a value?",
+  },
+  {
+    id: 60,
+    category: "technical",
+    subCategory: "Computer Science",
+    question:
+      "How would you find all the values for a specific key in an array of objects?",
+  },
+  {
+    id: 61,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "How do you interact with APIs on the front end?",
+  },
+  {
+    id: 62,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "Whats the difference between an HTTP request and response?",
+  },
+  {
+    id: 63,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "What does a 200 status code mean?",
+  },
+  {
+    id: 64,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "What does a 400 status code mean?",
+  },
+  {
+    id: 65,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "What is chaining in javascript?",
+  },
+  {
+    id: 66,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "How do you stay up-to-date on JS and React news/improvements?",
+  },
+  {
+    id: 67,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "Why use cookies?",
+  },
+  {
+    id: 68,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "Have you used local storage?",
+  },
+  {
+    id: 69,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "Whats the difference between let and const?",
+  },
+  {
+    id: 70,
+    category: "technical",
+    subCategory: "Computer Science",
+    question: "Whats your experience with React Hooks? Why use them?",
+  }
+]
