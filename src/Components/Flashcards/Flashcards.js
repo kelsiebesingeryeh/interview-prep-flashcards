6a808b3c4fdc0c5e6bf521e8015d8d9326049213
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-03-27 13:11:28 -0600

    Add landing page for technical questions:

--- src/Components/Flashcards/Flashcards.js
+++ src/Components/Flashcards/Flashcards.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Flashcards.css'
-import Card from '../Card/Card' 
+import Card from '../Card/Card'
+import { Link } from 'react-router-dom' 
 
 const Flashcards = ({ flashcards, category }) => {
     const technicalFlashcards = flashcards.filter(card => card.category === 'technical')
@@ -17,12 +18,49 @@ const Flashcards = ({ flashcards, category }) => {
     }
 
     return (
-        <div className='flashcardsContainer'>
-            {category === 'technical' ? displayFlashcards(technicalFlashcards) :
-                displayFlashcards(behavioralFlashcards)
-            }
-        </div>
+            <div className="flashcardsContainer">
+            <h2 className='subCategoryTitle'>Pick a topic to generate interview questions</h2>
+                <div className='buttonContainer'>
+                    <Link to="/:id">
+                    <button className="subCategoryButton">HTML/CSS</button>
+                    </Link>
+                    <Link to="/:id">
+                    <button className="subCategoryButton">
+                        Generic Computer Science
+                    </button>
+                    </Link>
+                    <Link to="/:id">
+                    <button className="subCategoryButton">JavaScript</button>
+                    </Link>
+                    <Link to="/:id">
+                    <button className="subCategoryButton">React</button>
+                    </Link>
+                </div>
+            </div>
     )
 }
 
 export default Flashcards
+
+
+//  const technicalFlashcards = flashcards.filter(card => card.category === 'technical')
+//     const behavioralFlashcards = flashcards.filter(card => card.category === 'behavioral')
+
+//     const displayFlashcards = (list) => {
+//        return list.map(flashcard => {
+//             return <Card 
+//             id={flashcard.id}
+//             key={flashcard.id}
+//             question={flashcard.question}
+//         />
+//         }) 
+//     }
+
+//     return (
+//         <div className='flashcardsContainer'>
+//             {category === 'technical' ? displayFlashcards(technicalFlashcards) :
+//                 displayFlashcards(behavioralFlashcards)
+//             }
+//         </div>
+//     )
+// }
