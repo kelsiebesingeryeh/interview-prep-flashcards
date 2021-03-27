471f68c02d101b652241dee0788eb5becda9c0b0
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-03-27 10:47:50 -0600

    Add link to technical and behavioral questions

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
       <section className='mainHomepage'>
         <h1>Frontend Interview Flashcards</h1>
         <h2>Choose your interview path below</h2>
-        <button>Behavioral</button>
-        <button>Technical</button>
+        <Link to='behavioral'>
+            <button>Behavioral</button>
+        </Link>
+        <Link to='technical'>
+            <button>Technical</button>
+        </Link>
       </section>
     )
 }
