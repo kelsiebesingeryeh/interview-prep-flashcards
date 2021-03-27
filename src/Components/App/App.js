81abae61b765469969e1850c26f3ffb615fc4d33
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-03-27 10:42:08 -0600

    Create skeleton homepage

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,23 +1,13 @@
-import './App.css';
+import './App.css'
+import React from 'react'
+import Home from '../Home/Home'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <main>
+     <Home />
+   </main>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+      <section className='mainHomepage'>
+        <h1>Frontend Interview Flashcards</h1>
+        <h2>Choose your interview path below</h2>
+        <button>Behavioral</button>
+        <button>Technical</button>
+      </section>
+    )
+}
+
+export default Home
