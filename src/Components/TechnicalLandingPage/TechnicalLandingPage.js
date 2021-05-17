0d3ccd3c4f075f58dc14669993696b7ebc6b3c4d
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-05-17 11:59:16 -0600

    Add link to back arrow to take you to home page

--- src/Components/TechnicalLandingPage/TechnicalLandingPage.js
+++ src/Components/TechnicalLandingPage/TechnicalLandingPage.js
@@ -7,21 +7,23 @@ const TechnicalLandingPage = ({ category }) => {
     return (
       <>
       <div className="flashcardsContainer">
-        <img src={backArrow} alt='back-arrow'/>
+        <Link to='/'>
+          <img src={backArrow} alt='back-arrow'/>
+        </Link>
         <h2 className="subCategoryTitle">
           Pick a topic to generate interview questions
         </h2>
         <div className="buttonContainer">
-          <Link to={`${category}/htmlCSS`}>
+          <Link to={`/${category}/htmlCSS`}>
             <button className="subCategoryButton">HTML/CSS</button>
           </Link>
-          <Link to={`${category}/computerScience`}>
+          <Link to={`/${category}/computerScience`}>
             <button className="subCategoryButton">Computer Science</button>
           </Link>
-          <Link to={`${category}/javascript`}>
+          <Link to={`/${category}/javascript`}>
             <button className="subCategoryButton">JavaScript</button>
           </Link>
-          <Link to={`${category}/react`}>
+          <Link to={`/${category}/react`}>
             <button className="subCategoryButton">React</button>
           </Link>
         </div>
