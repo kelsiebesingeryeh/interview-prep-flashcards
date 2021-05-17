0a3e967ce0d56e987d38df8726c1a0c3a6b51705
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-05-17 14:55:35 -0600

    Add linter to App component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -12,55 +12,56 @@ const App = () => {
   const getFlashcardIndex = (id, list) => list.findIndex((card) => card.id === id);
 
   return (
-   <main>
-     <Switch>
+    <main>
+      <Switch>
         <Route
-        exact
-        path='/'
-        render={() => <Home />}
+          exact
+          path="/"
+          render={() => <Home />}
         />
 
-        <Route 
-        exact
-        path='/:category'
-        render={({ match }) => {
-          if (match.params.category === 'technical') {
-            return (
-              <TechnicalLandingPage
-                category={match.params.category}
-              />
-            );
-          }
-          else if (match.params.category === 'behavioral') {
-            return (
-              <Flashcards
-                flashcards={flashcards}
-                subCategory={match.params.subCategory}
-                getFlashcardIndex={getFlashcardIndex}
-                category={match.params.category}
-              />
-            );
-          }
-        }}
+        <Route
+          exact
+          path="/:category"
+          render={({ match }) => {
+            if (match.params.category === 'technical') {
+              return (
+                <TechnicalLandingPage
+                  category={match.params.category}
+                />
+              );
+            } else if (match.params.category === 'behavioral') {
+              return (
+                <Flashcards
+                  flashcards={flashcards}
+                  subCategory={match.params.subCategory}
+                  getFlashcardIndex={getFlashcardIndex}
+                  category={match.params.category}
+                />
+              );
+            }
+          }}
         />
-      <Route 
-      exact path='/:category/:subCategory'
-      render={({ match }) => {
-        if (match.params.subCategory === 'htmlCSS' || match.params.subCategory === 'computerScience' || match.params.subCategory === 'javascript' || match.params.subCategory === 'react') {
-          return (
-            <Flashcards
-              flashcards={flashcards}
-              subCategory={match.params.subCategory}
-              getFlashcardIndex={getFlashcardIndex}
-              category={match.params.category}
-            />
-          );
-        }
-      }}
-      />
-     </Switch>
-   </main>
-  )
-}
+        <Route
+          exact
+          path="/:category/:subCategory"
+          render={({ match }) => {
+            if (match.params.subCategory === 'htmlCSS' || match.params.subCategory === 'computerScience'
+          || match.params.subCategory === 'javascript' || match.params.subCategory === 'react') {
+              return (
+                <Flashcards
+                  flashcards={flashcards}
+                  subCategory={match.params.subCategory}
+                  getFlashcardIndex={getFlashcardIndex}
+                  category={match.params.category}
+                />
+              );
+            }
+          }}
+        />
+      </Switch>
+    </main>
+  );
+};
 
 export default App;
