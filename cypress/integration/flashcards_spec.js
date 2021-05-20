084b5010b4ebcb4b093a84974e9a3c83cdb90cf9
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-05-20 11:40:46 -0600

    Add testing for all 4 buttons to make sure they route to the correct path

--- cypress/integration/flashcards_spec.js
+++ cypress/integration/flashcards_spec.js
@@ -45,7 +45,8 @@ it("should display a back arrow", () => {
 });
 
 it("should take you back to the homepage if you click the back arrow", () => {
-  cy.get("img").click().get(".mainHomepage").should("be.visible")
+  cy.get("img").click();
+  cy.url("http://localhost:3000").get(".mainHomepage").should("be.visible");
 });
 
 it("should display a left arrow", () => {
@@ -60,3 +61,58 @@ it("should allow a user to click on the right arrow and be taken to another ques
   cy.get(".right-arrow").click().get(".question").should("be.visible");
 });
 })
+
+describe("Technical Landing Page", () => {
+  beforeEach(() => {
+    const baseURL = "http://localhost:3000/technical";
+    cy.visit(baseURL);
+  });
+
+  it("should display a title", () => {
+    cy.get(".subCategoryTitle").should("be.visible");
+  });
+
+  it("should display a back arrow", () => {
+    cy.get("img").should("be.visible");
+  });
+
+  it("should take you back to the homepage if you click the back arrow", () => {
+    cy.get("img").click()
+    cy.url("http://localhost:3000").get(".mainHomepage").should("be.visible");
+  });
+
+  it("should display 4 different buttons for category paths", () => {
+    cy.get(".subCategoryButton").first().should("contain", "HTML/CSS");
+    cy.get(".subCategoryButton").eq(1).should("contain", "Computer Science");
+    cy.get(".subCategoryButton").eq(2).should("contain", "JavaScript");
+    cy.get(".subCategoryButton").last().should("contain", "React");
+  });
+
+  it("should allow a user to click on the HTML/CSS path and be taken to that page", () => {
+    cy.get(".subCategoryButton")
+      .first()
+      .click()
+      .url("http://localhost:3000/technical/htmlCSS");
+  });
+
+  it("should allow a user to click on the Computer Science path and be taken to that page", () => {
+    cy.get(".subCategoryButton")
+      .eq(1)
+      .click()
+      .url("http://localhost:3000/technical/computerScience");
+  });
+
+  it("should allow a user to click on the JavaScript path and be taken to that page", () => {
+    cy.get(".subCategoryButton")
+      .eq(2).click()
+      .url("http://localhost:3000/technical/javascript");
+  });
+
+  it("should allow a user to click on the React path and be taken to that page", () => {
+    cy.get(".subCategoryButton")
+      .last()
+      .click()
+      .url("http://localhost:3000/technical/react");
+  });
+
+});
